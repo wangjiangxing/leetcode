d67e6b33fa37fa9b59bb0b132534dd1948d4ecc1
wangjiangxing <wjx353042907@hotmail.com>
2021-03-14 14:53:53 +0800

    answer1

---
+++ 1.cpp
@@ -0,0 +1,30 @@
+//
+// Created by cruisin on 2021/3/14.
+//
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int flag=0;
+    int i=0;
+    int j=0;
+    int *ret=(int *)malloc(3*sizeof(int));
+    *returnSize=2;
+    for(int i=0;i<numsSize;i++){
+        for(j=i+1;j<numsSize;j++)
+        {
+            if(nums[i]+nums[j]==target)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==1)
+        {
+            ret[0]=i;
+            ret[1]=j;
+            break;
+        }
+    }
+    return ret;
+}
